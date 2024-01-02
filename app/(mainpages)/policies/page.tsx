7800efe7db9ef076871f7ea88a92cd8ae0141f45
Addison Wanlass <hi@addison.codes>
2024-01-02 16:32:54 -0500

    fix: new policies page

---
+++ app/(mainpages)/policies/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Policies from '@/components/Policies'
+
+
+const page = () => {
+  return (
+    <div>
+      <Policies />
+    </div>
+  )
+}
+
+export default page
--- components/EmployeeServices.tsx
+++ components/EmployeeServices.tsx
@@ -9,7 +9,7 @@ function EmployeeServices({}: Props) {
       <h2 className='mt-10 mb-4 text-4xl font-extrabold text-white font-universSubheading dark:text-white'><span className='ml-2'>Employee Services</span></h2>
       <div className="text-sm font-medium text-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
         <a
-          href="#"
+          href="/policies"
           aria-current="true"
           className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-aptpblue focus:outline-none focus:ring-2 focus:ring-aptpblue focus:text-aptpblue dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
         >
