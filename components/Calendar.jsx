ddec2e1a5a55a6b8ac6bd44ff4172788f6e3816d
Addison Wanlass <hi@addison.codes>
2024-01-02 16:12:02 -0500

    fix: calendar

--- components/Calendar.jsx
+++ components/Calendar.jsx
@@ -3,19 +3,15 @@ import React from 'react'
 const Calendar = () => {
   return (
       // <iframe src='https://outlook.office365.com/owa/calendar/7e396f32a8ba40ee8927aaada71549da@allianceptp.com/4f6c6e3ee57646ee8b62440a4022685213065606818718831078/calendar.html' width="100%" height="600"></iframe>
-<iframe
-  id="open-web-calendar"
-  style={{
-    background:
-      'url("https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif") center center no-repeat'
-  }}
-  src="https://open-web-calendar.hosted.quelltext.eu/calendar.html?url=https%3A%2F%2Foutlook.office365.com%2Fowa%2Fcalendar%2F7e396f32a8ba40ee8927aaada71549da%40allianceptp.com%2F4f6c6e3ee57646ee8b62440a4022685213065606818718831078%2Fcalendar.ics&amp;title=Alliance%20Physical%20Therapy%20Partners%20Event%20Calendar&amp;skin=dhtmlxscheduler_flat.css&amp;start_of_week=su&amp;css=%40media%20(max-width%3A%20500px)%20%7B%0A.dhx_cal_date%20%7B%0Adisplay%3A%20none%20!important%0A%7D%0A)"
+
+<iframe id="open-web-calendar" 
+    style="background:url('https://raw.githubusercontent.com/niccokunzmann/open-web-calendar/master/static/img/loaders/circular-loader.gif') center center no-repeat;"
+    src="https://open-web-calendar.hosted.quelltext.eu/calendar.html?url=https%3A%2F%2Foutlook.office365.com%2Fowa%2Fcalendar%2F7e396f32a8ba40ee8927aaada71549da%40allianceptp.com%2F4f6c6e3ee57646ee8b62440a4022685213065606818718831078%2Fcalendar.ics&amp;title=Alliance%20PTP%20Calendar&amp;skin=dhtmlxscheduler_flat.css&amp;start_of_week=su&amp;controls=next&amp;controls=previous&amp;tabs=month"
   sandbox="allow-scripts allow-same-origin allow-top-navigation"
   scrolling="no"
   frameBorder={0}
   height="515px"
-  width="100%"
-/>
+  width="100%" />
 
   )
 }
