8162eee055bb878718c4d1292d64afa4a3ee27cd
Addison Wanlass <hi@addison.codes>
2024-01-02 17:01:52 -0500

    feat: comment page

---
+++ app/(mainpages)/comment/page.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='container'>
+      <iframe
+        className='clickup-embed clickup-dynamic-height'
+        src='https://forms.clickup.com/42662264/f/18nybr-9913/CHX98RZLQJAD229FC6'
+        onWheel=''
+        width='100%'
+        height='100%'
+      ></iframe>
+      <script
+        async
+        src='https://app-cdn.clickup.com/assets/js/forms-embed/v1.js'
+      ></script>
+    </div>
+  )
+}
+
+export default page
