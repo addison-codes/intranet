c1e5373ac70d9dba388f792ed47723f7e8b5d318
Addison Wanlass <hi@addison.codes>
2024-01-03 01:47:43 -0500

    fix: rollback changes in layout

--- app/(homepage)/layout.jsx
+++ app/(homepage)/layout.jsx
@@ -11,7 +11,7 @@ import ReviewCarousel from '@/components/Carousel'
 import Script from 'next/script'
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
-import redirect from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
@@ -25,18 +25,18 @@ export const metadata = {
 export default async function RootLayout({
   children,
 }) {
-    const supabase = createServerComponentClient({ cookies })
-  const { data: { session } } = await supabase.auth.getSession()
+  //   const supabase = createServerComponentClient({ cookies })
+  // const { data: { session } } = await supabase.auth.getSession()
 
-  if (!session) {
-    redirect('/login')
-  }
+  // if (!session) {
+  //   redirect('/login')
+  // }
 
-  console.log(session.user)
-  if (session.user.email !== undefined) {
-    const user = session.user.email.split('@')
-    const name = user[0].split('.')
-  }
+  // console.log(session.user)
+  // if (session.user.email !== undefined) {
+  //   const user = session.user.email.split('@')
+  //   const name = user[0].split('.')
+  // }
 
   return (
     <html lang="en" className={`${GeistSans.className} w-screen`}>
