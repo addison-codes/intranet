2ca7c0b7bf8e61d27ad137f21039c9cafd42e552
Addison Wanlass <hi@addison.codes>
2024-01-04 11:40:27 -0500

    fix: small fixes

--- components/Policies.jsx
+++ components/Policies.jsx
@@ -6,44 +6,44 @@ const Policies = () => {
       <h2 className='mt-10 mb-4 text-6xl font-extrabold font-universHeading dark:text-white'><span className='ml-2'>Policies</span></h2>
       <div className="text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg dark:bg-gray-700 dark:border-gray-600 dark:text-white">
         <a
-          href="/0bfdf96d-8df8-4757-a866-d0fe072d3a17"
+          href="/pages/50"
           aria-current="true"
           className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-aptpblue focus:outline-none focus:ring-2 focus:ring-aptpblue focus:text-aptpblue dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
         >
           Inclement Weather Guidelines
         </a>
         <a
-          href="/8908a50b-83cd-488e-a931-c67f2fdb80c5"
+          href="/pages/104"
           className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-aptpblue focus:outline-none focus:ring-2 focus:ring-aptpblue focus:text-aptpblue dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
         >
           Employee Handbook
         </a>
         <a
-          href="/8908a50b-83cd-488e-a931-c67f2fdb80c5#689a915e5ff84d9cab0865fadcb11d3b"
+          href="/pages/105"
           className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-aptpblue focus:outline-none focus:ring-2 focus:ring-aptpblue focus:text-aptpblue dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
         >
           Attendance Policy
         </a>
         <a
-          href="/5e21547a-b5bc-40ae-b02a-0122023d0265"
+          href="/pages/106"
           className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-aptpblue focus:outline-none focus:ring-2 focus:ring-aptpblue focus:text-aptpblue dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
         >
           Requesting PTO
         </a>
         <a
-          href="/c57ff274-2016-407c-bfca-3d1d58e219a1"
+          href="/pages/107"
           className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-aptpblue focus:outline-none focus:ring-2 focus:ring-aptpblue focus:text-aptpblue dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
         >
           2023 Holiday Schedule
         </a>
         <a
-          href="/575ded7d-9543-4669-987d-9d713a053d36"
+          href="/pages/68"
           className="block w-full px-4 py-2 border-b border-gray-200 cursor-pointer hover:bg-gray-100 hover:text-aptpblue focus:outline-none focus:ring-2 focus:ring-aptpblue focus:text-aptpblue dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
         >
           Social Media Policy
         </a>
         <a
-          href="/9118cb1e-c342-4cfc-850d-837bbe263684"
+          href="/pages/108"
           className="block w-full px-4 py-2 border-b border-gray-200 rounded-b-lg cursor-pointer hover:bg-gray-100 hover:text-aptpblue focus:outline-none focus:ring-2 focus:ring-aptpblue focus:text-aptpblue dark:border-gray-600 dark:hover:bg-gray-600 dark:hover:text-white dark:focus:ring-gray-500 dark:focus:text-white"
         >
           Referral Bonus Policy
