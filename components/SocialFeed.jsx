90c3f7421b6b9124dd5b8491c3cabe5af457dd15
Addison Wanlass <hi@addison.codes>
2024-01-04 16:47:14 -0500

    fix: social scroll

--- components/SocialFeed.jsx
+++ components/SocialFeed.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const SocialFeed = () => {
 
   return (
-    <div className='col-span-4 row-span-2 overflow-y-scroll sm:col-span-1 h-96'>
+    <div className='h-full col-span-4 row-span-2 overflow-x-hidden overflow-y-scroll sm:col-span-1'>
       <script src="https://static.elfsight.com/platform/platform.js" data-use-service-core defer></script>
       <div class="elfsight-app-0497e85a-aa99-4e8e-987e-5df2efce507a" data-elfsight-app-lazy></div>
     </div>
