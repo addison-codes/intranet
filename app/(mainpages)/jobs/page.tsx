ffb786ec4652a6341aff73d9bfd278ef62a382e2
Addison Wanlass <hi@addison.codes>
2024-01-05 13:45:55 -0500

    chore: changed script layout

--- app/(mainpages)/jobs/page.tsx
+++ app/(mainpages)/jobs/page.tsx
@@ -1,12 +1,11 @@
 'use client'
 
 import React from 'react'
-import Script from 'next/script'
 
 const jobs = () => {
   return (
     <div>
-      <Script id="gnewtonjs" type="text/javascript" src="https://recruitingbypaycor.com/career/iframe.action?clientId=8a7883d0879c591b0187e3570b4e28cc" />
+      <script id="gnewtonjs" type="text/javascript" src="https://recruitingbypaycor.com/career/iframe.action?clientId=8a7883d0879c591b0187e3570b4e28cc" />
     </div>
   )
 }
