dfb8c223d73c408899442aecede9f439291a2cac
Addison Wanlass <hi@addison.codes>
2024-01-05 14:03:20 -0500

    fix: iframe height

--- app/(mainpages)/jobs/page.tsx
+++ app/(mainpages)/jobs/page.tsx
@@ -8,7 +8,7 @@ const jobs = () => {
       {/* <script id="gnewtonjs" type="text/javascript" src="https://recruitingbypaycor.com/career/iframe.action?clientId=8a7883d0879c591b0187e3570b4e28cc" /> */}
 <iframe
   id="gnewtonIframe"
-  // style={{ overflow: "scroll", height: 6000 }}
+  style={{ overflow: "scroll", height: 4000 }}
   src="https://recruitingbypaycor.com/career/CareerHome.action?clientId=8a7883d0879c591b0187e3570b4e28cc"
   width="100%"
   height="100%"
