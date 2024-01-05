4bc809d67d0fffe036557c03230d0c8de26699ee
Addison Wanlass <hi@addison.codes>
2024-01-05 13:54:13 -0500

    fix: changed script to iframe

--- app/(mainpages)/jobs/page.tsx
+++ app/(mainpages)/jobs/page.tsx
@@ -5,7 +5,15 @@ import React from 'react'
 const jobs = () => {
   return (
     <div>
-      <script id="gnewtonjs" type="text/javascript" src="https://recruitingbypaycor.com/career/iframe.action?clientId=8a7883d0879c591b0187e3570b4e28cc" />
+      {/* <script id="gnewtonjs" type="text/javascript" src="https://recruitingbypaycor.com/career/iframe.action?clientId=8a7883d0879c591b0187e3570b4e28cc" /> */}
+<iframe
+  id="gnewtonIframe"
+  style={{ overflow: "scroll", height: 800 }}
+  src="https://recruitingbypaycor.com/career/CareerHome.action?clientId=8a7883d0879c591b0187e3570b4e28cc"
+  width="100%"
+  frameBorder={0}
+  scrolling="yes"
+/>
     </div>
   )
 }
