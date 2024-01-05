4eb02b472d0ed65cd689b8b53489467e1789eda3
Addison Wanlass <hi@addison.codes>
2024-01-05 13:56:40 -0500

    fix: add height to iframe

--- app/(mainpages)/jobs/page.tsx
+++ app/(mainpages)/jobs/page.tsx
@@ -11,6 +11,7 @@ const jobs = () => {
   style={{ overflow: "scroll", height: 800 }}
   src="https://recruitingbypaycor.com/career/CareerHome.action?clientId=8a7883d0879c591b0187e3570b4e28cc"
   width="100%"
+  height="100%"
   frameBorder={0}
   scrolling="yes"
 />
