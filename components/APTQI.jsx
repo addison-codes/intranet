24a7d60ef26f9d787473443c97fd36c28f941679
Addison Wanlass <hi@addison.codes>
2024-02-22 15:15:19 -0500

    fix: colors and borders

--- components/APTQI.jsx
+++ components/APTQI.jsx
@@ -29,7 +29,7 @@ const APTQI = () => {
 
           <button
           type="button"
-          className="text-white bg-aptpred border-b border-gray-200 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ml-8 dark:bg-blue-600 dark:hover:bg-aptpblue focus:outline-none dark:focus:ring-blue-800 center"
+          className="text-white bg-aptpred border-none hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-bold rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-aptpblue focus:outline-none dark:focus:ring-blue-800 center"
         >
           <a href='https://www.aptqi.com/take-action/' rel='nofollow' target='_blank'>Take action alongside us</a>
         </button>
