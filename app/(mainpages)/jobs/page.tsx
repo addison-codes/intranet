4dd5a7d8fce193b601c841fbeed551227dcb83e1
Addison Wanlass <hi@addison.codes>
2024-05-29 10:21:29 -0400

    fix: internal jobs iframe

--- app/(mainpages)/jobs/page.tsx
+++ app/(mainpages)/jobs/page.tsx
@@ -6,7 +6,7 @@ const jobs = () => {
   return (
     <div>
       {/* <script id="gnewtonjs" type="text/javascript" src="https://recruitingbypaycor.com/career/iframe.action?clientId=8a7883d0879c591b0187e3570b4e28cc" /> */}
-<script id="gnewtonjs" type="text/javascript" src="https://recruitingbypaycor.com/career/internalIframe.action?internal=true&clientId=8a7883d0879c591b0187e3570b4e28cc&key=99611f096869b09151d9febaf25cd856"></script>
+<iframe id="gnewtonjs" src="https://recruitingbypaycor.com/career/internalIframe.action?internal=true&clientId=8a7883d0879c591b0187e3570b4e28cc&key=99611f096869b09151d9febaf25cd856" />
     </div>
   )
 }
